82f41878ad22f9b946c94f47e6c874e15c1bd071
qdslab2017 <37395397+qdslab2017@users.noreply.github.com>
2018-03-15 21:52:29 +0600

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('cur_convert', 'Cur_Controller@cur_convert');
+Route::get('multi_cur_convert', 'Cur_Controller@multi_cur_convert');
+Route::get('top_currency_list', 'Cur_Controller@top_currency_list');
+Route::post('updatecurrency', 'Cur_Controller@updatecurrency');
+Route::post('multiupdatecurrency', 'Cur_Controller@multiupdatecurrency');
+Route::get('/', function () {
+    return view('welcome');
+});
