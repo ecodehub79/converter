1bbb68b97ca3705d9734833dcc4a593c7ca72f4e
qdslab2017 <37395397+qdslab2017@users.noreply.github.com>
2018-03-16 12:31:57 +0600

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,13 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('historical', 'Cur_Controller@historical');
 Route::get('cur_convert', 'Cur_Controller@cur_convert');
 Route::get('multi_cur_convert', 'Cur_Controller@multi_cur_convert');
 Route::get('top_currency_list', 'Cur_Controller@top_currency_list');
 Route::post('updatecurrency', 'Cur_Controller@updatecurrency');
 Route::post('multiupdatecurrency', 'Cur_Controller@multiupdatecurrency');
+Route::post('historicalcurrency', 'Cur_Controller@historicalcurrency');
 Route::get('/', function () {
     return view('welcome');
 });
